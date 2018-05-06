5125e27fca7092ede2bed405d46da78a7cd33d9c
Ilya Averyanov <av@fun-box.ru>
2018-05-06 23:40:50 +0300

    Added package info to mix.exs

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule TokenBucket.Mixfile do
         "coveralls.detail": :test,
         "coveralls.post": :test,
         "coveralls.html": :test
-      ]
+      ],
+      package: package()
     ]
   end
 
@@ -28,6 +29,17 @@ defmodule TokenBucket.Mixfile do
     [
       {:excoveralls, "~> 0.8", only: :test},
       {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp package do
+    [
+      name: :token_bucket,
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Ilya Averyanov"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/savonarola/token_bucket"},
+      description: "Rate limiter implementing Token Bucket algorithm"
     ]
   end
 end
